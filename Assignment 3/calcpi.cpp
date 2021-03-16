c9ea7e91194e91dfdf8b5983d45c28503a802286
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-16 12:01:32 +0800

    Update calcpi.cpp
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update
    
    Update
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Revert "Update calcpi.cpp"
    
    This reverts commit 2ebd8709c15b1e52b0de8a1aca3515e3aef09780.
    
    Update calcpi.cpp
    
    Revert "Update calcpi.cpp"
    
    This reverts commit f911ae82a3d1895be3b19f410af9bd81edd4497c.
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Revert "Update calcpi.cpp"
    
    This reverts commit 274bbeaf229eef0ca0a470281a1b29f520770035.
    
    Update calcpi.cpp
    
    Revert "Update calcpi.cpp"
    
    This reverts commit af7a2d7f80a0190acb79890805015407b95dce93.
    
    Update calcpi.cpp
    
    Revert "Update calcpi.cpp"
    
    This reverts commit b130c674c808dfcb99fdc435c8063d196aec078f.
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Update calcpi.cpp
    
    Revert "Update calcpi.cpp"
    
    This reverts commit f6d6ff972dde7da24565230ac43f4a287b6f34da.
    
    pthread
    
    Update calcpi.cpp

--- Assignment 3/calcpi.cpp
+++ Assignment 3/calcpi.cpp
@@ -15,58 +15,88 @@
 // parallelize the function so that it uses n_threads threads to do
 // the computation.
 
+#include <pthread.h>
 #include <thread>
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include "calcpi.h"
 
 struct Task {
     int start_row;
     int end_row;
-    int count;
+    uint64_t count;
     int r;
+    double rsq;
 };
 Task tasks[256];
+pthread_t threads[256];
 
-uint64_t count_pixels(int r, int n_threads) {
-    int row_amount = std::ceil(r / n_threads);
 
-    uint64_t count = 0;
-    // for (double x = 1; x <= r; x++)
-    //     for (double y = 0; y <= r; y++)
-    //         if (x * x + y * y <= rsq) count++;
-
-    for (int t = 0; t < n_threads; t++) {
-        Task task = tasks[t];
-
-        task.r = r;
-        task.start_row = row_amount * t;
-        task.end_row = row_amount * (t + 1) - 1;
-
-        if (task.end_row > r) {
-            task.end_row = r;
-        }
-
-        std::thread t(count_pixels_thread, t);
-        t.join();
-    }
-
-    for (Task task : tasks) {
-        count += task.count;
-    }
-
-    return count * 4 - 3;
-}
-
-void count_pixels_thread(int t) {
-    Task task = tasks[t];
-    double rsq = double(task.r) * task.r; // r^2
-
-    for (double x = task.start_row; x <= task.end_row; x++) {
-        for (double y = 0; y <= task.r; y++) {
-            if (x * x + y * y <= rsq) {
+void count_pixels_thread(Task& task) {
+    // Counting
+    // Here we only calculate 1/4 of the circle, the right bottom corner
+    for (double x = task.start_row; x < task.end_row; x++) {
+        for (double y = 1; y <= task.r; y++) {
+            if (x * x + y * y <= task.rsq) {
                 task.count++;
             }
         }
     }
 }
+
+uint64_t count_pixels(int r, int n_threads) {
+
+    // If radius is 0 or a negative number (invalid), there is no circle, return 0
+    if (r <= 0) {
+        return 0;
+    }
+
+    // Over the limit of number of threads
+    if (n_threads > 256) {
+        n_threads = 256;
+    }
+
+    // Initialize local variables
+    int row_amount = r / n_threads;
+    uint64_t count = 0;
+    double rsq = double(r) * r; // r^2
+
+    // The number of threads is greater than radius, each thread will handle less than 1 row, 
+    // so we force each thread to handle minimum 1 row.
+    if (row_amount < 1) {
+        row_amount = 1;
+    }
+
+    // Launch threads and assign tasks
+    for (int t = 0; t < n_threads; t++) {
+        Task& task = tasks[t];
+
+        // Set radius, start and end row; (start, end]
+        task.r = r;
+        task.rsq = rsq;
+        task.start_row = row_amount * t;
+        task.end_row = row_amount * (t + 1);
+
+        // If start or end row is greater than radius, set to radius
+        if (task.start_row > r) task.start_row = r;
+        if (task.end_row > r) task.end_row = r;
+
+        // Append to threads list
+        // threads[t] = std::thread(count_pixels_thread, std::ref(task));
+        pthread_create(&threads[t], NULL, count_pixels_thread, &task);
+    }
+
+    // Join threads and count
+    for (int t = 0; t < n_threads; t++) {
+        // threads[t].join();
+        pthread_join(threads[t], NULL);
+        count += tasks[t].count;
+    }
+
+    return count * 4 + 1;
+}
+
+// ./calcpi 10 2
+// ./calcpi 2 2
+// ./calcpi 100000 16
