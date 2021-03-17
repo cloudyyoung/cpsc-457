f7da62d360f1c6792d6c68aef1f6c46cf8d88d79
Cloudy Young <cloudyyoung74@outlook.com>
2021-03-17 19:37:45 +0800

    Update calcpi.cpp

--- Assignment 3/calcpi.cpp
+++ Assignment 3/calcpi.cpp
@@ -38,6 +38,8 @@ void count_pixels_thread(Task& task) {
     uint64_t count = 0;
     int start_row = task.start_row;
     int end_row = task.end_row;
+    int r = ::r;
+    double rsq = ::rsq;
 
     for (double x = start_row; x < end_row; x++) {
         for (double y = 1; y <= r; y++) {
