0bac0d51f66a6e7ed6c2a64fa1f37828320e9e61
Cloudy Young <cloudyyoung74@outlook.com>
2021-04-17 13:11:17 +0800

    Update memsim.cpp
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Revert "Update memsim.cpp"
    
    This reverts commit 31d6663e3f2612a9b873f41bda8328dedd080bea.
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Revert "Update memsim.cpp"
    
    This reverts commit d60976e37a7055c4c67c5a98947441d0e85d5b0a.
    
    Update memsim.cpp
    
    Revert "Update memsim.cpp"
    
    This reverts commit 67e0f1959642d4a30c5cb5c5840d2becaf368ee4.
    
    Update memsim.cpp
    
    Revert "Update memsim.cpp"
    
    This reverts commit 10500a3b41ae99f368b83e8cad65676889ebdf70.
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Update memsim.cpp
    
    Removed print
    
    Update memsim.cpp
    
    Revert "Removed print"
    
    This reverts commit 28c7f1882b980d781c08c026172aeaf9f8ae4282.
    
    Update PartitionIterator
    
    Update unoccupied to set
    
    Update PartitionIterator

--- assignment-5/memsim/memsim.cpp
+++ assignment-5/memsim/memsim.cpp
@@ -7,6 +7,9 @@
 #include <cassert>
 #include <unordered_map>
 #include <list>
+#include <set>
+#include <iterator>
+#include <climits>
 
 // I suggest you implement the simulator as a class. This is only a suggestion.
 // Feel free to ignore it.
@@ -14,34 +17,61 @@ struct Partition {
   int tag;
   int64_t start;
   int64_t size;
-  Partition* predecessor;
-  Partition* successor;
 
-  Partition(int64_t start, int64_t size) {
-    this->tag = NULL;
+  Partition(int64_t start, int64_t size, int tag = 0) {
+    this->tag = tag;
     this->start = start;
     this->size = size;
-    this->predecessor = NULL;
-    this->successor = NULL;
   }
 
   bool occupied() {
-    return this->tag != NULL;
+    return this->tag != 0;
+  }
+};
+
+typedef std::list<Partition>::iterator PartitionIterator;
+typedef std::set<PartitionIterator>::iterator PartitionIteratorIterator;
+
+struct PartitionCompare {
+  bool operator()(const PartitionIterator& p1, const PartitionIterator& p2) const {
+    if (p1->size == p2->size) {
+      return p1->start < p2->start;
+    } else {
+      return p1->size > p2->size;
+    }
   }
 };
 
 struct Simulator {
   int64_t page_size;
   int64_t page_requested;
-  std::list<Partition> partitions;                                  // [Partition]
-  std::list<Partition*> unoccupied;                                 // [Partition Pointer]
-  std::unordered_map<int, std::list<Partition*>> tag_partitions;    // Tag: [Partition Pointer]
+  std::list<Partition> partitions;                                                    // [Partition]
+  std::set<PartitionIterator, PartitionCompare> unoccupied;                               // [Partition Iterator]
+  std::unordered_map<int, std::list<PartitionIterator>> tag_partitions;  // Tag: [Partition Iterator]
 
   Simulator(int64_t page_size) {
     this->page_size = page_size;
     this->page_requested = 0;
     this->partitions = std::list<Partition>();
-    this->unoccupied = std::list<Partition*>();
+    this->tag_partitions = std::unordered_map<int, std::list<PartitionIterator>>();
+  }
+
+  int64_t n_page_request(int64_t size) {
+    int64_t page_size = this->page_size;
+    int64_t remainder = size % page_size;
+
+    if (remainder == 0) {
+      return size / page_size;
+    } else {
+      return size / page_size + 1;
+    }
+  }
+
+  void unoccupied_erase_by_partition_iterator(PartitionIterator partition_iterator) {
+    PartitionIteratorIterator partition_iterator_iterator = this->unoccupied.find(partition_iterator);
+    if (partition_iterator_iterator != this->unoccupied.end()) {
+      this->unoccupied.erase(partition_iterator_iterator);
+    }
   }
 
   void allocate(int tag, int64_t size) {
@@ -60,96 +90,125 @@ struct Simulator {
 
     // If just started
     if (partitions.empty()) {
-      int64_t new_pages = size / this->page_size + 1;
+      int64_t new_pages = this->n_page_request(size);
       Partition head = Partition(0, new_pages * this->page_size);
       this->page_requested += new_pages;
 
+      std::cout << "  HEAD new pages: " << new_pages << ", head size: " << head.size << std::endl;
+      std::cout << "  - page request += " << new_pages << ", total requested: " << this->page_requested << std::endl;
+
       this->partitions.push_back(head);
-      this->unoccupied.push_back(&head);
+      this->unoccupied.insert(this->partitions.begin());
     }
 
     // Local variables
     bool has_suitable = false;
-    Partition* suitable;
+    PartitionIterator suitable;
 
-    if (!unoccupied.empty()) { // Have unoccupied partitions, find largest suitable partition
-      int64_t suitable_size = 0;
-
-      for (Partition* each : unoccupied) {
-        if (each->size > suitable_size) {
-          suitable_size = each->size;
-          suitable = each;
-          has_suitable = true;
-        }
+    if (!this->unoccupied.empty()) { // Have unoccupied partitions, find largest suitable partition
+      PartitionIteratorIterator max_unoccupied_iterator = this->unoccupied.begin();
+      PartitionIterator max_unoccupied = *max_unoccupied_iterator;
+      if (max_unoccupied->size >= size) {
+        has_suitable = true;
+        suitable = max_unoccupied;
+        this->unoccupied.erase(max_unoccupied_iterator);
       }
-    }
 
-    // If tag_partition for tag is not intialized, then initialize
-    if (!this->tag_partitions.contains(tag)) {
-      this->tag_partitions[tag] = std::list<Partition*>();
+      std::cout << "  has suitable? " << std::boolalpha << has_suitable << std::endl;
     }
 
     // Split partiton OR expand by page
     if (!has_suitable) {
+      std::cout << "STEP: EXPAND PAGE " << std::endl;
+
       // No suitable unoccupied partition, expand partition at the end
-      Partition last = *this->partitions.end();
-      int64_t total_last_size = 0;
+      PartitionIterator last = std::prev(this->partitions.end());
 
       // Last partition unoccupied, make its size to total last size
-      if (last.tag == NULL) {
-        total_last_size += last.size;
-      }
+      if (!last->occupied()) {
+        int64_t total_last_size = last->size;
 
-      // Add pages
-      int64_t new_pages = (size - last.size) / this->page_size + 1;
-      total_last_size += new_pages * this->page_size;
-      this->page_requested += new_pages;
+        // Add pages
+        int64_t new_pages = this->n_page_request(size - last->size);
+        total_last_size += new_pages * this->page_size;
+        this->page_requested += new_pages;
+        std::cout << "  - page request += " << new_pages << ", total requested: " << this->page_requested << std::endl;
 
-      // Occupy last partition
-      last.tag = tag;
-      last.size = size;
-      tag_partitions[tag].push_back(&last);
-      total_last_size -= size;
 
-      // Append Leftover size if there is any
-      if (total_last_size > 0) {
-        int64_t start = last.start + last.size;
-        Partition new_last = Partition(start, total_last_size);
+        // Occupy last partition
+        this->unoccupied_erase_by_partition_iterator(last);
+        last->tag = tag;
+        last->size = size;
+        this->tag_partitions[tag].push_back(last);
+        total_last_size -= size;
+        std::cout << "  - last - start: " << last->start << ", size: " << last->size << ", tag: " << last->tag << std::endl;
 
+
+        // Append Leftover size if there is any
+        if (total_last_size > 0) {
+          int64_t start = last->start + last->size;
+          Partition new_last = Partition(start, total_last_size);
+
+          this->partitions.push_back(new_last);
+          this->unoccupied.insert(std::prev(this->partitions.end()));
+        }
+
+      } else {
+        // Make new partition at the end by new page
+        int64_t new_pages = this->n_page_request(size);
+        this->page_requested += new_pages;
+        int64_t total_last_size = new_pages * this->page_size;
+        int64_t leftover_size = total_last_size - size;
+
+        std::cout << "  - page request += " << new_pages << ", total requested: " << this->page_requested << std::endl;
+
+        // New partition
+        int64_t new_last_start = last->start + last->size;
+        Partition new_last = Partition(new_last_start, size, tag);
         this->partitions.push_back(new_last);
-        this->unoccupied.push_back(&new_last);
+        this->tag_partitions[tag].push_back(std::prev(this->partitions.end()));
+        std::cout << "  - last.start: " << last->start << std::endl;
+        std::cout << "  - last_start: " << new_last_start << std::endl;
 
-        last.successor = &new_last; // connects
-        new_last.predecessor = &last;
+        // Append leftover partition
+        if (leftover_size > 0) {
+          int64_t leftover_start = new_last_start + size;
+          Partition leftover = Partition(leftover_start, leftover_size);
+          this->partitions.push_back(leftover);
+          this->unoccupied.insert(std::prev(this->partitions.end()));
+        }
+
       }
 
     } else { // Split partition
 
+      std::cout << "STEP: SPLIT PARTITION " << std::endl;
+      std::cout << "  - SUITABLE start: " << suitable->start << ", size: " << suitable->size << std::endl;
+
       int64_t total_suitable_size = suitable->size;
       int64_t leftover_size = total_suitable_size - size;
+
+      std::cout << "  - total suitable size: " << total_suitable_size << ", leftover size: " << leftover_size << std::endl;
+
 
       // Occupy partition
       suitable->tag = tag;
       suitable->size = size;
       this->tag_partitions[tag].push_back(suitable);
+      std::cout << "  - partition " << this->partitions.size() << std::endl;
+      std::cout << "  - tag partition " << this->tag_partitions[tag].size() << std::endl;
+
 
       // Append leftover partition
       if (leftover_size > 0) {
+        std::cout << "  step: add leftover" << std::endl;
+
         int64_t leftover_start = suitable->start + size;
         Partition leftover = Partition(leftover_start, leftover_size);
 
-        leftover.predecessor = suitable; // connects
-        leftover.successor = suitable->successor;
-        if (leftover.successor != NULL) {
-          (*leftover.successor).predecessor = &leftover;
-        }
-        suitable->successor = &leftover;
-
-        this->unoccupied.push_back(&leftover);
+        this->partitions.insert(std::next(suitable), leftover);
+        this->unoccupied.insert(std::next(suitable));
       }
-
-      // Remove occupied partition from occupied
-      this->unoccupied.remove(suitable);
     }
   }
 
@@ -160,37 +219,40 @@ struct Simulator {
     //         - mark the partition free
     //         - merge any adjacent free partitions
 
-    std::list<Partition*> partitions = this->tag_partitions[tag];
-    for (Partition* partition : partitions) {
-      partition->tag = NULL;
-      this->unoccupied.push_back(partition);
+    std::list<PartitionIterator> partitions = this->tag_partitions[tag];
 
-      // If has predecessor and predecessor is unoccupied
-      Partition* predecessor = partition->predecessor;
-      if (predecessor != NULL && predecessor->tag == NULL) {
-        // Predecessor is unoccupied, merge with partition
-        partition->start = predecessor->start;
-        partition->size += predecessor->size;
+    for (PartitionIterator partition : partitions) {
+      std::cout << "  - tag partitions: " << partition->start << std::endl;
 
-        Partition* predecessor_predecessor = predecessor->predecessor;
-        predecessor_predecessor->successor = partition;
-        partition->predecessor = predecessor_predecessor;
+      partition->tag = 0;
 
-        this->unoccupied.remove(predecessor);
+      // If has predecessor
+      if (partition != this->partitions.begin()) {
+        PartitionIterator predecessor = std::prev(partition);
+
+        // And predecessor is unoccupied, merge with partition
+        if (!predecessor->occupied()) {
+          partition->start = predecessor->start;
+          partition->size += predecessor->size;
+          this->partitions.erase(predecessor);
+          this->unoccupied_erase_by_partition_iterator(predecessor);
+        }
       }
 
-      // If has successor and successor is unoccupied
-      Partition* successor = partition->successor;
-      if (successor != NULL && successor->tag == NULL) {
-        // Successor is unoccupied, merge with partition
-        partition->size += successor->size;
+      // If has successor
+      if (partition != this->partitions.end()) {
+        PartitionIterator successor = std::next(partition);
 
-        Partition* successor_successor = successor->successor;
-        successor_successor->predecessor = partition;
-        partition->successor = successor_successor;
-
-        this->unoccupied.remove(successor);
+        // And and successor is unoccupied
+        if (!successor->occupied()) {
+          // Successor is unoccupied, merge with partition
+          partition->size += successor->size;
+          this->partitions.erase(successor);
+          this->unoccupied_erase_by_partition_iterator(successor);
+        }
       }
+
+      this->unoccupied.insert(partition);
     }
 
     this->tag_partitions[tag].clear();
@@ -199,18 +261,15 @@ struct Simulator {
   MemSimResult getStats() {
     MemSimResult result;
 
-    int64_t max_size = 0;
-    int64_t max_start = 0;
-    for (Partition* each : unoccupied) {
-      int64_t each_size = each->size;
-      int64_t each_start = each->start;
-      if (each_size >= max_size && each_start < max_start) {
-        max_size = each_size;
-        max_start = each_start;
-      }
+    if (this->unoccupied.empty()) {
+      result.max_free_partition_size = 0;
+      result.max_free_partition_address = 0;
+    } else {
+      PartitionIteratorIterator max_unoccupied_iterator = this->unoccupied.begin();
+      PartitionIterator max_unoccupied = *max_unoccupied_iterator;
+      result.max_free_partition_size = max_unoccupied->size;
+      result.max_free_partition_address = max_unoccupied->start;
     }
-    result.max_free_partition_size = max_size;
-    result.max_free_partition_address = max_start;
 
     result.n_pages_requested = this->page_requested;
     return result;
@@ -231,6 +290,29 @@ struct Simulator {
     // make sure that every partition in free_blocks is actually free
 
     // make sure that none of the partition sizes or addresses are < 1
+
+    std::cout << "=======================" << std::endl;
+    std::cout << "partition size " << this->partitions.size() << std::endl;
+    for (Partition each : this->partitions) {
+      std::cout << "  partition [ start: " << each.start << ", size: " << each.size << ", tag: " << each.tag << " ]" << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "unoccupied size " << this->unoccupied.size() << std::endl;
+    for (PartitionIterator each : this->unoccupied) {
+      std::cout << "  unoccupied [ start: " << each->start << ", size: " << each->size << " ]" << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "page request " << this->page_requested << std::endl;
+    std::cout << "=======================" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
   }
 };
 
@@ -245,7 +327,11 @@ struct Simulator {
 MemSimResult mem_sim(int64_t page_size, const std::vector<Request>& requests)
 {
   Simulator sim(page_size);
+  std::cout << "page size: " << page_size << std::endl << std::endl;
+
   for (const auto& req : requests) {
+    std::cout << "request: " << req.tag << ", size: " << req.size << std::endl;
+
     if (req.tag < 0) {
       sim.deallocate(-req.tag);
     } else {
